2469b0922b7eb978d870f3a68151f9a1d5a54f0e
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2021-11-02 14:24:27 +0530

    Update SearchBook.js

--- screens/SearchBook.js
+++ screens/SearchBook.js
@@ -1,21 +1,87 @@
 import React from 'react';
-import {View,Text,ScrollView,FlatList} from 'react-native';
-import db from '../config';
-class SearchBook extends React.Component{
+import {View,Text,ScrollView,FlatList,TextInput,StyleSheet,TouchableOpacity} from 'react-native';
+import db from '../config'
 
+
+
+
+class SearchBook extends React.Component{
   constructor(){
     super();
-    this.state={allTransactions:[]}
+    this.state={
+       
+      allTransactions:[],
+      lastVisibleTransaction:null,
+      search:''
+
+
+    }
   }
-  
   componentDidMount=async()=>{
-    var query= await db.collection("transactions").get();
-    query.docs.map((doc)=>{
-      this.setState({allTransactions:[...this.state.allTransactions,doc.data()]})
+    // var transactions=await db.collection("transactions").limit(10).get();
+    // transactions.docs.map((doc)=>{
+    //  this.setState({
+    //    allTransactions:[...this.state.allTransactions,doc.data()],
+    //    lastVisibleTransaction:doc
+    //  })
+
+    // })
+
+  
+
+
+  }
+  fetchMoreTransactions=async()=>{
+  var transactions=await db.collection("transactions").startAfter(this.state.lastVisibleTransaction).limit(10).get();
+  
+  transactions.docs.map((doc)=>{
+    this.setState({
+      allTransactions:[...this.state.allTransactions,doc.data()],
+      lastVisibleTransaction:doc
     })
 
-  }
+   })
+}
+searchTransaction=async()=>{
 
+var enteredText=this.state.search.split("");
+console.log(enteredText[0]);
+if(enteredText[0].toUpperCase()==='B'){
+
+  var transaction=await db.collection("transactions").where("bookId","==",this.state.search).get();
+  transaction.docs.map((doc)=>{
+    this.setState({
+      allTransactions:[...this.state.allTransactions,doc.data()],
+      lastVisibleTransaction:doc
+    })
+  })
+
+
+
+
+
+
+}
+else if(enteredText[0].toUpperCase()==='S'){
+
+  var transaction=await db.collection("transaction").where("studentId","==",this.state.search).get();
+  transaction.docs.map((doc)=>{
+    this.setState({
+      allTransactions:[...this.state.allTransactions,doc.data()],
+      lastVisibleTransaction:doc
+    })
+  })
+
+}
+
+
+
+
+
+
+
+
+}
 
 
 
@@ -23,45 +89,116 @@ class SearchBook extends React.Component{
 
 
   render(){
-    return(
-      <ScrollView>
-      {this.state.allTransactions.map((data,index)=>{
-        return(
-        <View key={index} style={{alignItems:'center'}}>
-       <Text>{"Book ID:"+data.bookId}</Text>
-       <Text>{"Student ID:"+data.studentId}</Text>
-       <Text>{"Date:"+data.date.toDate()}</Text>
-       <Text>{"Transaction Type:"+data.transactionType}</Text>
+   return(
+       <View style={{marginTop:100}} >
+         {/* <ScrollView>
+         {this.state.allTransactions.map((item)=>{
+           return( 
+             <View style={{marginTop:23}}>  
+           <Text>{'BookId: '+item.bookId}</Text>
+           <Text>{'StudentId: '+item.studentId}</Text>
+           <Text>{'Date: '+item.date.toDate()}</Text>
+           <Text>{'transactionType: '+item.transactionType}</Text>
+           </View>
+           )
 
-       </View>)
-      })}
-       
-       </ScrollView>
-    )
-    //    <FlatList
-    //     data={this.state.allTransactions}
-    //    renderItem={({item})=>{
-    //      return(
-    //         <View style={{alignItems:'center',borderBottomWidth:2,marginTop:50,paddingBottom:50}}>
-    //    <Text>{"Book ID:"+item.bookId}</Text>
-    //    <Text>{"Student ID:"+item.studentId}</Text>
-    //    <Text>{"Date:"+item.date.toDate()}</Text>
-    //    <Text>{"Transaction Type:"+item.transactionType}</Text>
+         })}
 
-    //    </View>)
+         </ScrollView> */}
+
+         <View>
+           <TextInput
+           style={styles.TextInput}
+           placeholder="Enter BookId or StudentId"
+           onChangeText={
+             (text)=>{
+               this.setState({
+                 search:text
+               })
+             }
+           }
+           />
+           <TouchableOpacity style={styles.SrcBtn} onPress={this.searchTransaction}>
+             <Text style={styles.srchTxt}>Search</Text>
+           </TouchableOpacity>
+         </View>
+
+
+        <FlatList
+         data={this.state.allTransactions}
+         renderItem={({item})=>{
+
+          return( 
+            <View style={{marginTop:23}}>  
+          <Text>{'BookId: '+item.bookId}</Text>
+          <Text>{'StudentId: '+item.studentId}</Text>
+          <Text>{'Date: '+item.date.toDate()}</Text>
+          <Text>{'transactionType: '+item.transactionType}</Text>
+          </View>
+          )
+
+
+
+
+
+         }} 
          
-    //    }}
-    //    keyExtractor={(item,index)=>index.toString()}
-    //    >
+         onEndReached={this.fetchMoreTransactions}
+         onEndReachedThreshold={0.5}
         
-       
-    
-       
-    //    </FlatList>
+        
+        
+        
+        
+        
+        />
 
-    // );
+
+
+
+
+
+
+
+
+
+         
+       </View>
+       
+
+
+   );
+
+
+
+      
+
+ 
 
   }
 }
+
+const styles= StyleSheet.create({
+  TextInput:{
+    backgroundColor:"skyblue",
+    margin:20,
+    padding:20,
+    borderRadius:24
+  },
+  SrcBtn:{
+    padding:10,
+    marginTop:25,
+    backgroundColor:"yellow",
+    width:200,
+    alignSelf:"center",
+    borderRadius:8,
+    alignItems:'center',
+
+  },
+  srchTxt:{
+    fontSize:30,
+    fontFamily:"serif"
+  }
+})
 
 export default SearchBook;
